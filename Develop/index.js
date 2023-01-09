02d24d714219ee9ad1529dab376ba1553c7ae068
Hayden Dillon-Lloyd <115908599+rayjohn666@users.noreply.github.com>
2023-01-08 22:54:08 -0800

    readme, index.js
    added readme, and code for index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,76 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+//internal Modules
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {  
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username'
+    },
+
+    {   
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.'
+    }, 
+
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project'
+    },
+
+    {   
+        type: 'input',
+        name: 'description',
+        message: 'Description of your project'
+    },
+
+    {  
+        type: 'input',
+        name: 'installation',
+        message: 'Instructions to install your application'
+    },
+
+    {   
+        type: 'input',
+        name: 'usage',
+        message: 'How is this application to be used?'
+    },
+
+    {  
+        type: 'input',
+        name: 'contribute',
+        message: 'Instructions on how to contribute to your project'
+    },
+
+    {   
+        type: 'input',
+        name: 'test',
+        message: 'How would one test this application?'
+    },
+
+    {   
+        type: 'list',
+        name: 'license',
+        choices: ['MIT License', 'Apache 2.0 License', 'GNU General Public License v3.0']
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+const initReadme = () => {
+    inquirer.prompt(questions).then((answers) => {
+        fs.writeFile("test.md", generateMarkdown(answers), (err) => {
+        err ? console.error(err) : console.log('Your README has been generated!');
+        })
+    })
+}
 // Function call to initialize app
-init();
+initReadme();
--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Professional README Generator Starter Code
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+# README.generator
+  
+  ## Description
+  A command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribute](#contributing)
+  - [License](#license)
+  - [Tests](#tests)
+
+  ## Installation
+  To install this project, clone the repository to your local machine.
+
+  ## Usage
+  To use this application after installation, open the integrated terminal on the index.js file and run 'node index.js'. Answer the questions and a readme will be generated for you!
+
+  ## License
+  This app is covered under the [MIT License](https://opensource.org/licenses/MIT)<br>
+  
+
+
+  ## Tests
+  N/A
+
